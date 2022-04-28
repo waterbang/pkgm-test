02c17ac80ccd6650a7d3a0f9a25da937f9728193
waterbang <409071692@qq.com>
2022-04-28 14:09:43 +0800

    :dizzy: :construction_worker: 测试

--- packages/mod-body/#bfsp.ts
+++ packages/mod-body/#bfsp.ts
@@ -5,6 +5,7 @@ export default defineConfig((info) => {
     exports: {
       ".": "./index.ts",
     },
+    deps: ["mod-face"],
     packageJson: {
       license: "MIT",
       author: "BFChainer",
